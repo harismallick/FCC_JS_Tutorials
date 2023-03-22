fd2aef0dee2fc08e5e550a674544ac7e23ec3914
harismallick <mallick.haris@gmail.com>
2023-03-22 02:38:22 +0000

    first commit

---
+++ test.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+const a = 5;
+const b = 4;
+const c = a + b;
+console.log(`The answer is ${c}`);
+// document.body.innerHTML += `Answer to ${a} + ${b} is ${c}`
+
+// Comments are made in JS with double forward slashes
+// Need to install nodejs to execute js files in the terminal.
+// Alternatively, you can execute js files in the browser by importing into a html file.
+// The equivalent of print() from Python is console.log(). This will print the output in the terminal.
+// However, console.log() does not print the output on an html page.
+// Need to use document.body.innerHTML or document.getElementById.innerHTML
+
+/*
+Data types in JS:
+undefined, null, boolean, string, symbol, number and object
+*/
+
